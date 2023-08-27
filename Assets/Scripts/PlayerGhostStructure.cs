267c2aa4be129fb7ebc50974672a75165a27a38f
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-08-27 15:11:18 +0200

    Do TP Check before can TP;

---
+++ Assets/Scripts/PlayerGhostStructure.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGhostStructure : MonoBehaviour
+{
+
+    public Collider2D MyCollider;
+    public bool isTPBlocked = false;
+    public int LayerCheckFor = 6;
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,49 +7,54 @@ using UnityEngine.PlayerLoop;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float SpeedMultiplier = 10;
-
+    public PlayerTPLocationDefinition PlayerTP;
+    public static float SpeedMultiplier = 10;
+    
     public Boolean isInPast = true;
-    public float futureTPOffset = 70;
+    public static float futureTPOffset = 70;
+
     private Rigidbody2D playerBody ;
-    // Start is called before the first frame update
+
+    public GameObject PlayerLocationMarker;
+
+    public float TeleportSafeZone = 1.0f;
+
     void Start()
     {
         playerBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
-    void OnMove(InputValue inputValue)
-    {
-        playerBody.velocity = SpeedMultiplier * inputValue.Get<Vector2>();
-        Debug.Log("LOL");
-    }
-
     public void OnMoveButton( InputAction.CallbackContext cbc){
-        Debug.Log(" Should do move thing");
         playerBody.velocity = SpeedMultiplier * cbc.ReadValue<Vector2>();
     }
 
     public void OnAbilityButton( InputAction.CallbackContext cbc){
-        Debug.Log("Do Ability Here");
-        if ( isInPast ) {
-            this.gameObject.transform.position = new Vector3(
-                this.gameObject.transform.position.x + this.futureTPOffset,
-                this.gameObject.transform.position.y ,
-                this.gameObject.transform.position.z
-            );
+        Debug.Log("Ability Use");
+
+        Debug.Log(
+            Physics2D.IsTouchingLayers(PlayerTP.Collider,LayerMask.GetMask("GameWalls"))
+        );
+
+        if (Physics2D.IsTouchingLayers(PlayerTP.Collider,LayerMask.GetMask("GameWalls"))) {
+            return;
         }
-        else {
-            this.gameObject.transform.position = new Vector3(
-                this.gameObject.transform.position.x - this.futureTPOffset,
-                this.gameObject.transform.position.y ,
-                this.gameObject.transform.position.z
-            );
-        }
+            
+        this.transform.position = getTPLocation( this.gameObject, isInPast);
         isInPast = !isInPast;
     }
 
-    void Update() {
+    private static Vector3 getTPLocation( GameObject player, Boolean isInPast){
+        return new Vector3(
+            player.gameObject.transform.position.x + 
+                (isInPast ? PlayerMovement.futureTPOffset : - PlayerMovement.futureTPOffset) ,
 
+            player.gameObject.transform.position.y,
+            player.gameObject.transform.position.z
+        );
+        
+    }
+
+    private void FixedUpdate(){
+        PlayerLocationMarker.transform.position = getTPLocation( this.gameObject, isInPast);
     }
 }
