8f1ce874965c087d4a4e19e042f8cc9246ecdcdb
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-08-28 20:13:35 +0200

    Update some textures;

--- Assets/Scripts/ScreenToPlayerCam.cs
+++ Assets/Scripts/ScreenToPlayerCam.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class ScreenToPlayerCam : MonoBehaviour
 {
     public GameObject cameraTarget;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     void FixedUpdate(){
         this.transform.position = new Vector3( cameraTarget.transform.position.x, cameraTarget.transform.position.y, this.transform.position.z);
