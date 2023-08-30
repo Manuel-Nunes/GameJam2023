74e9fefa529c48d49976e3d398d876dc3cda59d4
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-08-31 01:11:47 +0200

    Add more animations for the character;

---
+++ Assets/Scripts/MountAToB.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MountAToB : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject Passenger;
+
+    public bool MountX = true;
+    public bool MountY = true;
+    public bool MountZ = true;
+
+    public Vector3 MountOffset = new Vector3(0,0,0);
+
+    public void Start(){
+        // if (MountOffset == null)
+        // {
+        //     MountOffset = new Vector3(0,0,0);
+        // }
+    }
+
+    public void FixedUpdate(){
+        this.Passenger.transform.position = new Vector3(
+            ( MountX ? this.Target.transform.position.x : this.Passenger.transform.position.x ) + MountOffset.x,
+            ( MountY ? this.Target.transform.position.y : this.Passenger.transform.position.y ) + MountOffset.y,
+            ( MountZ ? this.Target.transform.position.z : this.Passenger.transform.position.z ) + MountOffset.z
+        );
+    }
+}
