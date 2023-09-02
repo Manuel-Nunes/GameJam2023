99dddd4c6e300a9d469aed7bdb8bb41d266075e2
Raaga Krishna <raaga@bbd.co.za>
2023-09-02 19:42:17 +0200

    Custom died message

--- Assets/Scripts/GameEndController.cs
+++ Assets/Scripts/GameEndController.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameEndController : MonoBehaviour
 {
     private bool isPlayerDead = false;
-    private GameObject gameEndedPanel;
+    public GameObject gameEndedPanel;
+    public Text gameEndedText;
 
     void Start()
     {
-        gameEndedPanel = GameObject.FindGameObjectWithTag("Game End Panel");
-        gameEndedPanel.SetActive(false);
+        if (this.name.Equals("Dead Zone"))
+        {
+            gameEndedText.text = "You fell through a hole in the floor";
+        }
+        else if (this.name.Equals("Fire"))
+        {
+            gameEndedText.text = "The fire burned you...";
+        }
+
+        gameEndedPanel?.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
