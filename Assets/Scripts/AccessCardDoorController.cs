ba83967994e64b78db7d1e532b8f16c5f5dd1c9e
Raaga Krishna <raaga@bbd.co.za>
2023-09-02 08:59:52 +0200

    Access Card Door controller

---
+++ Assets/Scripts/AccessCardDoorController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessCardDoorController : MonoBehaviour
+{
+    private bool isAccessCardPickedUp = false;
+    private GameObject accessCard;
+    public Animator[] AccessCardDoorAnimators;
+    
+    public AudioClip DoorOpen;
+
+    private AudioSource source;
+
+    void Start()
+    {
+        accessCard = GameObject.FindGameObjectWithTag("Access Card");
+
+        // audio 
+        source = gameObject.AddComponent<AudioSource>();
+
+        // access card door set to closed 
+        foreach (Animator accessCardDoorAnimator in AccessCardDoorAnimators)
+            accessCardDoorAnimator.SetBool("DoorIsOpen", false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !isAccessCardPickedUp)
+            OpenDoor();
+    }
+
+    private void OpenDoor()
+    {
+        // play door open music
+        source.clip = DoorOpen;
+        source.Play();
+
+        // set access card picked up to true
+        isAccessCardPickedUp = true;
+
+        // remove access card from map 
+        Destroy(accessCard, source.clip.length);
+
+        // opening door
+        foreach (Animator DoorAnimator in AccessCardDoorAnimators)
+            DoorAnimator.SetBool("DoorIsOpen", true);
+    }
+}
